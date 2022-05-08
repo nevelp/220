863a33c988880744acc21258162316b16247a001
nevelp <98722479+nevelp@users.noreply.github.com>
2022-05-08 16:01:05 -0400

    Merge remote-tracking branch 'origin/main'

---
+++ HW5.sql
@@ -0,0 +1,42 @@
+create table customers
+(
+    id        int auto_increment
+        primary key,
+    firstname varchar(20) null,
+    lastname  varchar(20) null,
+    company   varchar(20) null,
+    constraint id
+        unique (id)
+)
+    auto_increment = 6;
+
+create table orders
+(
+    id         int auto_increment
+        primary key,
+    product    varchar(20) null,
+    cost       int         null,
+    customerId int         null,
+    constraint id
+        unique (id),
+    constraint orders_ibfk_1
+        foreign key (customerId) references customers (id)
+)
+    auto_increment = 6;
+
+create table customers_orders_association
+(
+    id         int not null
+        primary key,
+    customerId int null,
+    orderId    int null,
+    constraint customers_orders_association_customers_id_fk
+        foreign key (customerId) references customers (id),
+    constraint customers_orders_association_orders_id_fk
+        foreign key (orderId) references orders (id)
+);
+
+create index customerId
+    on orders (customerId);
+
+
